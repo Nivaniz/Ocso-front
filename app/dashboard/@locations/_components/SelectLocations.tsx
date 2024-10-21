17e94d475f37e3b0a5b878208befe1d8bc2a7bc1
Nivaniz <nivaniz2019@gmail.com>
2024-10-20 18:27:56 -0600

    Vídeo clase 77

--- app/dashboard/@locations/_components/SelectLocations.tsx
+++ app/dashboard/@locations/_components/SelectLocations.tsx
@@ -9,9 +9,9 @@ export default function SelectLocation({ locations, store }: { locations: Locati
         <Select placeholder="Selecciona una tienda" label="Tienda" classNames={{
             mainWrapper: "hover:ring-2 ring-red-300 rounded-xl transition-all"
         }}
-        selectedKeys={store ? store : ""}
+        selectedKeys={store ? store : "0"}
             onChange={((e) =>{
-                if (e.target.value === ""){
+                if (e.target.value === "0" || e.target.value === ""){
                     router.push(`/dashboard`)
                 } else {
                     router.push(`/dashboard?store=${e.target.value}`)
--- app/dashboard/@locations/page.tsx
+++ app/dashboard/@locations/page.tsx
@@ -9,12 +9,22 @@ const LocationsPage = async ({searchParams} : {
 }) => {
     const userCookies = cookies();
     const token = userCookies.get(TOKEN_NAME)?.value;
-    const {data} = await axios.get<Location[]>("http://127.0.0.1:4000/locations", {
+    let {data} = await axios.get<Location[]>("http://127.0.0.1:4000/locations", {
         headers: {
             Authorization: `Bearer ${token}`
         }
     });
 
+    data = [
+        {
+            locationId: 0,
+            locationName: "Ninguna",
+            locationLatLng: [0,0],
+            locationAddress: "No existe"
+        },
+        ... data
+    ]
+    
     return (
         <div className="w-8/12">
             <div className="w-full flex flex-col items-center h-[90vh] bg-red-50">
