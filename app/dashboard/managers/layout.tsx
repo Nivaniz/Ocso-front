11462eb1ff5fde7fcd7fc13addbba774f9d52608
Nivaniz <nivaniz2019@gmail.com>
2024-11-01 17:00:28 -0600

    Vídeo 97

--- app/dashboard/managers/layout.tsx
+++ app/dashboard/managers/layout.tsx
@@ -1,14 +1,14 @@
 import { ReactNode } from "react";
 import ManagerCards from "./_components/ManagerCards";
 
-export default function LayoutManagers({children}: {children: ReactNode}) {
+export default function LayoutManagers({children, count}: {children: ReactNode, count: ReactNode}) {
     return (
         <>
         <div className="w-4/12 max--h-[90vh] h-[90vh] overflow-hidden overflow-y-auto">
             <ManagerCards/>
         </div>
         <div className="w-7/12">
-            {children}
+            {children} {count}
         </div>
         </>
     )
