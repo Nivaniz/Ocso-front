1cec78ca5ef73afb77c2808ae60871e40401994b
Nivaniz <nivaniz2019@gmail.com>
2024-11-01 17:20:49 -0600

    Vídeo 98

--- app/dashboard/managers/@count/page.tsx
+++ app/dashboard/managers/@count/page.tsx
@@ -1,27 +1,44 @@
 import { API_URL } from "@/constants";
 import { Manager } from "@/entities";
 import { authHeaders } from "@/helpers/authHeaders";
+import { Card } from "@nextui-org/react";
 
 export default async function CountManagersPage(){
-    const response = await fetch(`${API_URL}/managers`,{
+    const response = await fetch(`${API_URL}/managers`, {
         headers: {
             ...authHeaders()
         },
         next: {
             tags: ["dashboard:managers"]
         }
-    })
-    const managers: Manager[] = await response.json()
+    });
+    
+    const managers: Manager[] = await response.json();
     const countNoStore = managers.filter((manager: Manager) => !manager.location).length;
+    const countWithStore = managers.length - countNoStore;
+    
     let max = 0;
+    let min = Infinity;
+    let salary = 0;
+    
     managers.forEach((manager: Manager) => {
         if (manager.managerSalary > max) max = manager.managerSalary;
-    })
+        if (manager.managerSalary < min) min = manager.managerSalary;
+        salary += manager.managerSalary;
+    });
+    
+    const averageSalary = (salary / managers.length).toFixed(2);
+    const noStorePercentage = ((countNoStore / managers.length) * 100).toFixed(2);
+    
     return (
-        <div className="font-bold">
-            <h1> Hay {managers.length} manager{managers.length > 1 ? "s" : ""}{""}</h1>
-            <h1> Hay {countNoStore} sin tienda</h1>
+        <Card className="w-fit px-2 py-4 text-center">
+            <h1> Hay {managers.length} manager{managers.length > 1 ? "s" : ""}</h1>
+            <h1> Hay {countNoStore} sin tienda ({noStorePercentage}%)</h1>
+            <h1> Hay {countWithStore} con tienda</h1>
             <h1> El salario máximo es {max} </h1>
-        </div>
-    )
+            <h1> El salario mínimo es {min} </h1>
+            <h1> El salario promedio es {averageSalary} </h1>
+            <h1> Rango de salarios: {max - min} </h1>
+        </Card>
+    );
 }
--- app/dashboard/managers/layout.tsx
+++ app/dashboard/managers/layout.tsx
@@ -7,8 +7,8 @@ export default function LayoutManagers({children, count}: {children: ReactNode, 
         <div className="w-4/12 max--h-[90vh] h-[90vh] overflow-hidden overflow-y-auto">
             <ManagerCards/>
         </div>
-        <div className="w-7/12">
-            {children} {count}
+        <div className="w-7/12 flex flex-col justify-center items-center gap-10">
+            <div>{children}</div> <div>{count}</div>
         </div>
         </>
     )
