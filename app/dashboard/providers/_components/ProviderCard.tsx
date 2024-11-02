8bc67069c95edb3b6076d61ecb7dd1c2a67e50df
Nivaniz <nivaniz2019@gmail.com>
2024-11-02 14:34:31 -0600

    Vídeo 107

---
+++ app/dashboard/providers/_components/ProviderCard.tsx
@@ -0,0 +1,26 @@
+import { Provider } from "@/entities";
+import { Card, CardBody, CardHeader, Divider } from "@nextui-org/react";
+
+export default function ProviderCard ({provider} : {provider: Provider}) {
+    return (
+        <Card className="w-4/12">
+            <CardHeader>{provider.providerName}</CardHeader>
+            <Divider/>
+            <CardBody>
+                <p>
+                    Correo electrónico: <b>{provider.providerEmail}</b>
+                </p>
+                <p>
+                    Numero de teléfono: <b>{provider.providerPhoneNumber}</b>
+                </p>
+                {
+                    provider.products ? (
+                        <p>
+                            Tiene <b>{provider.products.length}</b> productos
+                        </p>
+                    ) : <p> No tiene productos asignados </p>
+                }
+            </CardBody>
+        </Card>
+    );
+}
--- app/dashboard/providers/page.tsx
+++ app/dashboard/providers/page.tsx
@@ -1,5 +1,29 @@
-const ProviderPage = () => {
-    return null;
-}
+import { API_URL } from "@/constants";
+import { Provider } from "@/entities";
+import { authHeaders } from "@/helpers/authHeaders";
+import ProviderCard from "./_components/ProviderCard";
+import Link from "next/link";
+
+const ProviderPage = async () => {
+    const response = await fetch(`${API_URL}/providers`, {
+        headers: {
+            ...authHeaders()
+        }
+    });
+    const providers: Provider[] = await response.json();
+
+    return (
+        <div className="w-full flex-row flex-wrap px-10">
+            {providers.map((provider: Provider) => (
+                <Link
+                    key={provider.providerId}
+                    href={{ pathname: `/dashboard/providers/${provider.providerId}` }}
+                >
+                    <ProviderCard provider={provider} />
+                </Link>
+            ))}
+        </div>
+    );
+};
 
 export default ProviderPage;
