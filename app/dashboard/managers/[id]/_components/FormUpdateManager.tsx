e062cb8a17b828079923decd440045657856f7b3
Nivaniz <nivaniz2019@gmail.com>
2024-11-01 19:35:56 -0600

    Vídeo 104

--- app/dashboard/managers/[id]/_components/FormUpdateManager.tsx
+++ app/dashboard/managers/[id]/_components/FormUpdateManager.tsx
@@ -1,9 +1,18 @@
 import updateManager from "@/actions/managers/update";
 import { Manager } from "@/entities";
 import { Button, Input } from "@nextui-org/react";
+import SelectStore from "./SelectStore";
+import { API_URL } from "@/constants";
+import { authHeaders } from "@/helpers/authHeaders";
 
-export default function FormUpdateManager({ manager }: { manager: Manager }) {
+export default async  function FormUpdateManager({ manager }: { manager: Manager }) {
     const updateManagerWithId = updateManager.bind(null, manager.managerId);
+    const responseStores = await fetch(`${API_URL}/locations`, {
+        headers: {
+            ...authHeaders()
+        }
+    })
+    const stores = await responseStores.json()
     
     return (
         <form action={updateManagerWithId} className="bg-orange-400 rounded-md">
@@ -12,7 +21,7 @@ export default function FormUpdateManager({ manager }: { manager: Manager }) {
             <Input defaultValue={manager.managerEmail} placeholder="marco@gmail.com" name="managerEmail" />
             <Input defaultValue={String(manager.managerSalary)} placeholder="Salario" name="managerSalary" />
             <Input defaultValue={manager.managerPhoneNumber} placeholder="4439829922" name="managerPhoneNumber" />
-
+            <SelectStore stores={stores} defaultStore={manager?.location?.locationId} />
             <Button color="primary" type="submit">
                 Actualizar
             </Button>
--- app/dashboard/managers/[id]/page.tsx
+++ app/dashboard/managers/[id]/page.tsx
@@ -5,6 +5,7 @@ import { Card, CardBody, CardHeader, Divider } from "@nextui-org/react";
 import ManagerCard from "./_components/ManagerCard";
 import DeleteManagerButton from "./_components/DeleteManagerButton";
 import FormUpdateManager from "./_components/FormUpdateManager";
+import UpdateManager from "./_components/UpdateManager";
 
 export default async function ManagerPage({
     params,
@@ -26,8 +27,10 @@ export default async function ManagerPage({
         <div className="flex flex-col gap-10 flex-grow-0 items-center justify-center">
             <ManagerCard manager={data}></ManagerCard>
             <div className="bg-white shadow-medium rounded-md px-10 py-2">
+                <UpdateManager>
+                    <FormUpdateManager manager={data} />
+                </UpdateManager>
                 <DeleteManagerButton managerId={data.managerId} />
-                <FormUpdateManager manager={data} />
             </div>
         </div> 
     )
