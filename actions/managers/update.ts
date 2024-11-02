3666006d1937e52e4a3135e4f96364cedbdf2f45
Nivaniz <nivaniz2019@gmail.com>
2024-11-02 13:26:58 -0600

    Vídeo 105

--- actions/managers/update.ts
+++ actions/managers/update.ts
@@ -1,23 +1,37 @@
 "use server";
 
-import { API_URL} from "@/constants";
+import { API_URL } from "@/constants";
 import { authHeaders } from "@/helpers/authHeaders";
 import { revalidateTag } from "next/cache";
- 
-export default async function updateManager(managerId: string, formData: FormData){
-    let manager: any = {}
-    for (const key of formData.keys()){
-        manager[key] = formData.get(key);       
+import { redirect } from "next/navigation";
+
+export default async function updateManager(managerId: string, formData: FormData) {
+    let manager: any = {};
+    for (const key of formData.keys()) {
+        manager[key] = formData.get(key);
     }
-    const response = await fetch(`${API_URL}/managers/${managerId}}`, {
+
+    // Convertir `managerSalary` y `location` a números, si están presentes
+    manager['managerSalary'] = +manager['managerSalary'];
+    if (manager['location']) {
+        manager['location'] = +manager['location'];
+    } else {
+        delete manager['location'];
+    }
+
+    const response = await fetch(`${API_URL}/managers/${managerId}`, {
         method: "PATCH",
         body: JSON.stringify(manager),
         headers: {
+            'content-type': 'application/json',
             ...authHeaders(),
         },
-    })
-    if(response.status === 200){
-        revalidateTag("dashboard:managers")
-        revalidateTag(`dashboard:managers:${managerId}`)
-    } 
+    });
+
+
+    if (response.status === 200) {
+        revalidateTag("dashboard:managers");
+        revalidateTag(`dashboard:managers:${managerId}`);
+        redirect ("/dashboard/managers")
+    }
 }
--- app/dashboard/managers/[id]/_components/FormUpdateManager.tsx
+++ app/dashboard/managers/[id]/_components/FormUpdateManager.tsx
@@ -15,12 +15,12 @@ export default async  function FormUpdateManager({ manager }: { manager: Manager
     const stores = await responseStores.json()
     
     return (
-        <form action={updateManagerWithId} className="bg-orange-400 rounded-md">
+        <form action={updateManagerWithId} className="bg-orange-400 rounded-md flex flex-col flex-grow-0 gap-2">
             <h1>Actualizar Manager</h1>
-            <Input defaultValue={manager.managerFullName} placeholder="Marco Aurelio" name="managerFullName" />
-            <Input defaultValue={manager.managerEmail} placeholder="marco@gmail.com" name="managerEmail" />
-            <Input defaultValue={String(manager.managerSalary)} placeholder="Salario" name="managerSalary" />
-            <Input defaultValue={manager.managerPhoneNumber} placeholder="4439829922" name="managerPhoneNumber" />
+            <Input required={true} isRequired label="Nombre completo" defaultValue={manager.managerFullName} placeholder="Marco Aurelio" name="managerFullName" />
+            <Input required={true} isRequired label="Correo Electrónico" defaultValue={manager.managerEmail} placeholder="marco@gmail.com" name="managerEmail" />
+            <Input required={true} isRequired label="Salario" defaultValue={String(manager.managerSalary)} placeholder="5654" name="managerSalary" />
+            <Input required={true} isRequired label="Número de teléfono" defaultValue={manager.managerPhoneNumber} placeholder="4439829922" name="managerPhoneNumber" />
             <SelectStore stores={stores} defaultStore={manager?.location?.locationId} />
             <Button color="primary" type="submit">
                 Actualizar
