631520185e43eeea817f979c0d9041f4f3170598
Nivaniz <nivaniz2019@gmail.com>
2024-11-03 14:11:37 -0600

    Vídeo 111

--- app/dashboard/providers/_components/ProviderCard.tsx
+++ app/dashboard/providers/_components/ProviderCard.tsx
@@ -1,25 +1,25 @@
 import { Provider } from "@/entities";
 import { Card, CardBody, CardHeader, Divider } from "@nextui-org/react";
 
-export default function ProviderCard ({provider} : {provider: Provider}) {
+export default function ProviderCard({ provider }: { provider: Provider }) {
     return (
-        <Card className="w-full min-w-[350px]">
-            <CardHeader>{provider.providerName}</CardHeader>
-            <Divider/>
+        <Card className="w-full min-w-[350px] max-w-[350px]">
+            <CardHeader><b>{provider.providerName}</b></CardHeader>
+            <Divider />
             <CardBody>
                 <p>
                     Correo electrónico: <b>{provider.providerEmail}</b>
                 </p>
                 <p>
-                    Numero de teléfono: <b>{provider.providerPhoneNumber}</b>
+                    Número de teléfono: <b>{provider.providerPhoneNumber}</b>
                 </p>
-                {
-                    provider.products.length !== 0 ? (
-                        <p>
-                            Tiene <b>{provider.products.length}</b> productos{provider.products.length > 1 ? "s" : ""}
-                        </p>
-                    ) : <p> No tiene productos asignados </p>
-                }
+                {provider?.products?.length ? (
+                    <p>
+                        Tiene <b>{provider.products.length}</b> producto{provider.products.length > 1 ? "s" : ""}
+                    </p>
+                ) : (
+                    <p>No tiene productos asignados</p>
+                )}
             </CardBody>
         </Card>
     );
