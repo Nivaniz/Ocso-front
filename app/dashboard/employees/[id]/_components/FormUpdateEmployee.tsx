9943c272c01b5de7452104268a9b421e85e8593a
Nivaniz <nivaniz2019@gmail.com>
2024-11-10 18:21:58 -0600

    Vídeo 130

---
+++ app/dashboard/employees/[id]/_components/FormUpdateEmployee.tsx
@@ -0,0 +1,16 @@
+import { Employee } from "@/entities";
+import { Button, Input } from "@nextui-org/react";
+
+export default function  FormUpdateEmployee({employee}: {employee: Employee}){
+    return (
+        <form className="flex flex-col gap-2 p-9 bg-orange-500 rounded-md md-2 h-fit">
+            <Input label="Nombre" name="employeeName" defaultValue={employee.employeeName} />
+            <Input label="Apellidos" name="employeeLastName" defaultValue={employee.employeeLastName} />
+            <Input label="Correo electrónico" name="employeeEmail" defaultValue={employee.employeeEmail} />
+            <Input label="Num. de Teléfono" name="employeePhoneNumber" defaultValue={employee.employeePhoneNumber} />
+            <Button type="submit" color="primary">
+                Actualizar datos
+            </Button>
+        </form>
+    )
+}
--- app/dashboard/employees/[id]/page.tsx
+++ app/dashboard/employees/[id]/page.tsx
@@ -1,5 +1,25 @@
+import { API_URL } from "@/constants";
+import EmployeeCard from "../_components/EmployeeCard";
+import { authHeaders } from "@/helpers/authHeaders";
+import { Employee } from "@/entities";
+import FormUpdateEmployee from "./_components/FormUpdateEmployee";
+
 export default async function EmployeesPage({params} : {params: {id: string}}) {
-    return params.id;
+    const respondeEmployee = await fetch(`${API_URL}/employees/${params.id}`,{
+        headers: {
+            ...authHeaders(),
+        },
+    });
+    const employee: Employee = await respondeEmployee.json();
+    return (
+        <div className="w-full h-[90vh] flex flex-row">
+            <div>
+                <EmployeeCard employee={employee} />
+            </div>
+            <div>
+                <FormUpdateEmployee employee={employee}/>
+            </div>
+        </div>
+    )
 }
 
-// return <EmployeeCard employee={employee}>;
