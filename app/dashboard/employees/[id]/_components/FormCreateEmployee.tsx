ae27bb9c8988d3b52caed2cee75b2b8614083517
Nivaniz <nivaniz2019@gmail.com>
2024-11-15 19:48:00 -0600

    Vídeo 136

--- app/dashboard/employees/[id]/_components/FormCreateEmployee.tsx
+++ app/dashboard/employees/[id]/_components/FormCreateEmployee.tsx
@@ -1,13 +1,23 @@
 import createEmployee from "@/actions/employees/create";
+import { API_URL } from "@/constants";
+import { authHeaders } from "@/helpers/authHeaders";
 import { Button, Input } from "@nextui-org/react";
+import SelectLocations from "./SelectLocation";
 
-export default function FormCreateEmployee(){
+export default async function FormCreateEmployee(){
+    const responseLocations = await fetch(`${API_URL}/locations`, {
+        headers: {
+            ...authHeaders()
+        }
+    })
+    const locations = await responseLocations.json();
     return (
         <form action={createEmployee} className="flex flex-col gap-2 p-9 bg-orange-500 rounded-md md-2 h-fit">
             <Input isRequired={true} label="Nombre" name="employeeName" placeholder="Marco"/>
             <Input isRequired={true} label="Apellidos" name="employeeLastName" placeholder="Aurelio"/>
             <Input isRequired={true} label="Correo electrónico" name="employeeEmail"  placeholder="marco@gmail.com"/>
             <Input isRequired={true} label="Num. de Teléfono" name="employeePhoneNumber"  placeholder="444XXXXX"/>
+            <SelectLocations stores={locations} />
             <Button type="submit" color="primary">
                 Crear Empleado
             </Button>
--- app/dashboard/employees/[id]/_components/FormUpdateEmployee.tsx
+++ app/dashboard/employees/[id]/_components/FormUpdateEmployee.tsx
@@ -1,16 +1,26 @@
 import updateEmployee from "@/actions/employees/update";
+import { API_URL } from "@/constants";
 import { Employee } from "@/entities";
+import { authHeaders } from "@/helpers/authHeaders";
 import { Button, Input } from "@nextui-org/react";
+import SelectLocations from "./SelectLocation";
 
-export default function  FormUpdateEmployee({employee}: {employee: Employee}){
+export default async function  FormUpdateEmployee({employee}: {employee: Employee}){
     const {employeeId} = employee;
     const updateEmployeeById = updateEmployee.bind(null, employeeId);
+    const responseLocations = await fetch(`${API_URL}/locations`, {
+        headers: {
+            ...authHeaders()
+        }
+    })
+    const locations = await responseLocations.json();
     return (
         <form action={updateEmployeeById} className="flex flex-col gap-2 p-9 bg-orange-500 rounded-md md-2 h-fit">
             <Input label="Nombre" name="employeeName" defaultValue={employee.employeeName} />
             <Input label="Apellidos" name="employeeLastName" defaultValue={employee.employeeLastName} />
             <Input label="Correo electrónico" name="employeeEmail" defaultValue={employee.employeeEmail} />
             <Input label="Num. de Teléfono" name="employeePhoneNumber" defaultValue={employee.employeePhoneNumber} />
+            <SelectLocations stores={locations} defaultStore={employee.location?.locationId}/>
             <Button type="submit" color="primary">
                 Actualizar datos
             </Button>
